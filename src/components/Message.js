ae44ea3acb3963ecdef22b4c4399d0be9fdf2b84
srikanth gurram <2gurram@gmail.com>
2020-03-28 20:07:38 +0530

    Using State in Class Component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class Message extends Component{
+    constructor(){
+        super()
+        this.state = {
+            message: 'Welcome Visitor'
+        }
+    }
+
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for Subscribing'
+        })
+    }
+
     render(){
         return(
-            <h3>Welcome Visitor</h3>
+            <div>
+                <h3>{this.state.message}</h3>
+                <button onClick={() => {this.changeMessage()}}>Subscribe</button>
+            </div>
         )
     }
 }
