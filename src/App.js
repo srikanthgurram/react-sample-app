c7530be4343b724a12744764630ac3d50f3dcdee
srikanth gurram <2gurram@gmail.com>
2020-03-28 19:43:44 +0530

    Initial commit with seperate Components for Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopNavbar from './components/TopNavBar'
+import MainPageIntro from './components/MainPageIntro'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNavbar appName="My React App"></TopNavbar>
+      <MainPageIntro></MainPageIntro>
+      <Message></Message>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Message extends Component{
+    render(){
+        return(
+            <h3>Welcome Visitor</h3>
+        )
+    }
+}
+
+export default Message
