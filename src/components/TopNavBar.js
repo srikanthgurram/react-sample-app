cc945e341e7698a37e75da49d259c500353a4c4a
srikanth gurram <2gurram@gmail.com>
2020-03-29 15:53:32 +0530

    Using destructuring props to update Brand Name in Top Nav bar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const TopNavBar = props => {
+    const {appName} = props
     return(
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a class="navbar-brand" href="#">Fixed navbar</a>
+          <a class="navbar-brand" href="#">{appName}</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
