5abf677cff8f3176a24d8194b656b23d62764102
srikanth gurram <2gurram@gmail.com>
2020-03-29 15:41:28 +0530

    Using set state and call back to modify the state values

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import TopNavbar from './components/TopNavBar'
 import MainPageIntro from './components/MainPageIntro'
-import Message from './components/Message'
+import Subscription from './components/Subscriptions'
 
 function App() {
   return (
     <div className="App">
       <TopNavbar appName="My React App"></TopNavbar>
       <MainPageIntro></MainPageIntro>
-      <Message></Message>
+      <Subscription></Subscription>
     </div>
   );
 }
--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
-class Message extends Component{
+class Subscription extends Component{
     constructor(){
         super()
         this.state = {
-            message: 'Welcome Visitor'
+            message: 'Welcome Visitor',
+            count: 0
         }
     }
 
+
+
     changeMessage(){
         this.setState({
-            message: 'Thank you for Subscribing'
+            message: 'Thank you for Subscribing',
+            count: this.state.count + 1
+        }, () => {
+            console.log("Total Visitors: ", this.state.count)
         })
     }
 
@@ -18,10 +24,11 @@ class Message extends Component{
         return(
             <div>
                 <h3>{this.state.message}</h3>
+                <h4>Total number of Visitors: {this.state.count}</h4>
                 <button onClick={() => {this.changeMessage()}}>Subscribe</button>
             </div>
         )
     }
 }
 
-export default Message
+export default Subscription
